60ede31e3c442445a3fb17fbe09b8fdb9843df65
Péter Gombos <gombos@pvv.ntnu.no>
2013-01-22 10:05:55 +0100

    playing with malloc

--- ex1/src/rpn.c
+++ ex1/src/rpn.c
@@ -2,27 +2,34 @@
 #include <stdlib.h>
 
 RpnCalc newRpnCalc(){
-    RPNCalc calc;
+    RpnCalc calc = (RpnCalc*)malloc(sizeof(RpnCalc));
     calc.size = 64;
     calc.top = 0;
-    calc.stack = (double)malloc(size*sizeof(double));
+    calc.stack = (double*)malloc(calc.size*sizeof(double));
     return calc;
 }
 
 void push(RpnCalc* rpnCalc, double n){
-    // TODO push n on stack, expand stack if neccesary
     if(rpnCalc.top == rpnCalc.size){
-        // TODO expand stack to twice the size
-        rpnCalc.size = 2*rpnCalc.size;
+        rpnCalc.size *= 2;
+        rpnCalc.stack = (double*)realloc(rpnCalc.stack, rpnCalc.size*sizeof(double));
     }
-    rpnCalc[++top] = n;
-    return;
+    rpnCalc[++rpnCalc.top] = n;
 }
 
 void performOp(RpnCalc* rpnCalc, char op){
-    // TODO perform operation
+    double num1 = rpnCalc.stack[rpnCalc.top--];
+    double num2 = rpnCalc.stack[rpnCalc.top--];
+    double result;
+    switch(op){
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+    }
+    push(rpnCalc, result);
 }
 
 double peek(RpnCalc* rpnCalc){
-    // TODO return top element of stack
+    return rpnCalc.stack[rpnCalc.top];
 }
