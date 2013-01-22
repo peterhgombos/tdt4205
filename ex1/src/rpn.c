905c872ee66d391abce2aa49cf76a13b18b1bb39
Péter Gombos <gombos@pvv.ntnu.no>
2013-01-22 09:37:31 +0100

    implemented newRpnCalc and started push

--- ex1/src/rpn.c
+++ ex1/src/rpn.c
@@ -1,12 +1,22 @@
 #include "rpn.h"
+#include <stdlib.h>
 
 RpnCalc newRpnCalc(){
-    // TODO initialize new RpnCalc
-    // find out how big the stack needs to be
+    RPNCalc calc;
+    calc.size = 64;
+    calc.top = 0;
+    calc.stack = (double)malloc(size*sizeof(double));
+    return calc;
 }
 
 void push(RpnCalc* rpnCalc, double n){
     // TODO push n on stack, expand stack if neccesary
+    if(rpnCalc.top == rpnCalc.size){
+        // TODO expand stack to twice the size
+        rpnCalc.size = 2*rpnCalc.size;
+    }
+    rpnCalc[++top] = n;
+    return;
 }
 
 void performOp(RpnCalc* rpnCalc, char op){
